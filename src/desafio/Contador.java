16d6533f26bf1768a36133c0e657301bde449ad0
sntgrazi <sntgrazi22@gmail.com>
2023-09-12 17:17:42 -0300

    desafio pronto

---
+++ src/desafio/Contador.java
@@ -0,0 +1,30 @@
+package desafio;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = parametroUm; i < parametroDois; i++) {
+            System.out.println(i);
+        }
+    }
+}
